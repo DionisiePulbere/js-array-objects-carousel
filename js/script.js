849e19f995a6d211a7786b9405689875d4a9ac0d
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-22 02:04:04 +0100

    feat: add com

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@
 // e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per 
 // l'ultima miniatura se l'utente clicca la freccia verso il basso.
 
+// scriviamo un array contentente oggetti
 const images = [
     {
         image: 'img/01.webp',
@@ -42,13 +43,13 @@ const images = [
 
 // valore active di default
 let activeImg = 0;
-
+// attribuiamo le costanti collegate al DOM
 const mainImgContainer = document.querySelector('#main-container');
 const sideImgContainer = document.querySelector('#side-container');
+// creiamo un ciclo for per far girare le immagini
 for (let i = 0; i < images.length; i++) {
     const thisImage = images[i];
-    console.log(thisImage.image);
-
+    // creiamo il path da satampare nel DOM img grande
     const newImage = `
     <div id="text" class="position-absolute text-white bottom-0 end-0 text-end pe-3">
         <h3>${thisImage.title}</h3>
@@ -57,7 +58,7 @@ for (let i = 0; i < images.length; i++) {
     <img class="main" src="${thisImage.image}" alt="${thisImage.title}"> 
     `
     mainImgContainer.innerHTML += newImage;
-
+    // creiamo il path da satampare nel DOM img grande
     const newSide = `
     <img class="side" src="${thisImage.image}" alt="${thisImage.title}">
     `
@@ -70,28 +71,28 @@ const text = document.querySelectorAll('#text');
 text[activeImg].classList.add('active');
 const allSide = document.querySelectorAll('.side');
 allSide[activeImg].classList.add('active');
-
+// definiamo il click della freccia in basso 
 const down = document.querySelector('.down');
 down.addEventListener('click', function (){
-
+    // al click, rimuoviamo la classe active 
     document.querySelector('.main.active').classList.remove('active');
     document.querySelector('#text.active').classList.remove('active');
     document.querySelector('.side.active').classList.remove('active');
+    // creiamo un ciclo for per incrementare active di 1 
     if (activeImg < allImages.length - 1) {
         activeImg++;
     } else {
         activeImg = 0;
     }
-    console.log(activeImg);
-
-allImages[activeImg].classList.add('active');
-text[activeImg].classList.add('active');
-allSide[activeImg].classList.add('active');
+    // attribuiamo active all'oggetto successivo contenente nell'array
+    allImages[activeImg].classList.add('active');
+    text[activeImg].classList.add('active');
+    allSide[activeImg].classList.add('active');
 });
-
+// definiamo il click della freccia in alto 
 const up = document.querySelector('.up');
 up.addEventListener('click', function() {
-
+    // al click, rimuoviamo la classe active 
     document.querySelector('.main.active').classList.remove('active');
     document.querySelector('#text.active').classList.remove('active');
     document.querySelector('.side.active').classList.remove('active');
@@ -101,7 +102,7 @@ up.addEventListener('click', function() {
     } else {
         activeImg = allImages.length - 1;
     }
-
+    // attribuiamo active all'oggetto precedente contenente nell'array
     allImages[activeImg].classList.add('active');
     text[activeImg].classList.add('active');
     allSide[activeImg].classList.add('active');
